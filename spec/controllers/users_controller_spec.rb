06cc47de624a0688d828946fff8c5b5d9a27dd39
Elliott Grieco <elliott.grieco@nyu.edu>
2018-09-04 14:23:47 -0400

    Adds routes and spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  before { get :index, params: params }
+  subject { JSON.parse(response.body) }
 
+  let(:query) { "this is a query" }
+  let(:params) { { query: query } }
+
+  context 'with a query' do
+    it { should eql("query" => "this+is+a+query") }
+  end
+
+  context 'without a query' do
+    let(:query) { nil }
+
+    it { should eql("query" => '') }
+  end
 end
